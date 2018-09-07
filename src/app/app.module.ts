e85247bd6b10db2ab09c147be74f10fc6fb2da33
Balu <Balakrishna_Gottipati@abercrombie.com>
2018-09-07 14:02:58 -0400

    added child module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderModule } from '../../child/src/app/modules/header/header.module';
+import { ButtonModule } from '../../child/src/app/modules/button/button.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeaderModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
